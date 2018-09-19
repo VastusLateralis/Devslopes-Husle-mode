4a2504d07deb7419673bc3e2b0423e237d1435f0
Thomas Johannessen <thomasjohannessen@thomass-macbook-pro.local>
2018-09-19 19:55:17 +0200

    Initial Commit

---
+++ Devslopes Husle mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Devslopes Husle mode
+//
+//  Created by Thomas Johannessen on 19/09/2018.
+//  Copyright © 2018 Thomas Johannessen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
